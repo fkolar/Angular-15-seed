19878b7af4e0f8554cf07411f683d677ad99b356
Frantisek kolar <fkolar@kolar.pro>
2016-03-17 14:54:00 +0100

    Initial commit

---
+++ config/build.config.js
@@ -0,0 +1,31 @@
+var fs = require('fs');
+var pkg = require('../package.json');
+
+module.exports = {
+    filename: 'my.app.js',
+    banner: '/*!\n' +
+    ' * This is example of my License\n' +
+    ' */\n',
+
+    closureStart: '(function(){\n use strict;',
+    closureEnd: '\n})();',
+
+    dev: 'www',
+    release: 'dist',
+
+    others: ['www/images/*.*'],
+    resetDir: ['www/js', 'www/css', 'dist', 'www/lib', 'node_modules'],
+    cleanJS: ['js/*.js'],
+    cleanCSS: ['css/*.css'],
+    srcJS: ['src/app/**/*.js'],
+    srcCSS: ['src/my.app.scss'],
+    srcHTML: ['src/**/*.html'],
+    combineFiles: ['www/index.html'],
+
+    testFiles: ['test/**/*.js'],
+
+    versionData: {
+        version: pkg.version
+    }
+};
+
---
+++ src/app/app.js
@@ -0,0 +1,34 @@
+var myApp = angular.module('superApp', [
+    'ngNewRouter',
+    'templatescache'
+]);
+
+myApp.config(['$componentLoaderProvider', function ($componentLoaderProvider) {
+
+    $componentLoaderProvider.setTemplateMapping(function (name) {
+
+        // app we want to keep in the root as base place holder
+        if (name === 'app') {
+            return 'app/'+ name + '.html';
+        } else if (name.toLowerCase().indexOf('layout') > 0) {
+            return 'common/layout/' + name + '/' + name + '.html';
+        }
+        return 'common/components/' + name + '/' + name + '.html';
+    });
+}]);
+
+myApp.controller('AppController', function ($router) {
+    var vm = this;
+    vm.name = 'Angular Developer';
+
+
+    /**
+     * Routing configuration
+     */
+    $router.config([
+        { path:'/', component:'app' }
+    ]);
+});
+
+
+
